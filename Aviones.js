aae5e63c2723c645953a21b9d36271af1e8b7d0a
PabloBB <91970935+PabloBerBol@users.noreply.github.com>
2022-01-25 14:14:28 +0100

    Añadidos aviones :)

---
+++ Aviones.js
@@ -0,0 +1,73 @@
+
+    let Aviones = [
+        {
+            "origen":"Palma",
+            "destino": "Oslo",
+            "horasalida": "15:00",
+            "horallegada": "18:00",
+            "url": "http://www.ibserveis.com/vuelo1.html"
+        },
+        {
+            "origen":"Lugo",
+            "destino": "Madrid",
+            "horasalida": "10:00",
+            "horallegada": "11:00",
+            "url": "http://www.ibserveis.com/vuelo2.html"
+        },
+        {
+            "origen":"Barna",
+            "destino": "Paris",
+            "horasalida": "20:00",
+            "horallegada": "22:00",
+            "url": "http://www.ibserveis.com/vuelo3.html"
+        },
+        {
+            "origen":"Palma",
+            "destino": "Madrid",
+            "horasalida": "18:00",
+            "horallegada": "20:00",
+            "url": "http://www.ibserveis.com/vuelo4.html"
+        },
+        {
+            "origen":"Palma",
+            "destino": "Paris",
+            "horasalida": "18:00",
+            "horallegada": "21:00",
+            "url": "http://www.ibserveis.com/vuelo5.html"
+        },
+        {
+            "origen":"Oslo",
+            "destino": "Barcelona",
+            "horasalida": "07:00",
+            "horallegada": "13:00",
+            "url": "http://www.ibserveis.com/vuelo6.html"
+        },
+        {
+            "origen":"Berlin",
+            "destino": "Tokio",
+            "horasalida": "10:00",
+            "horallegada": "23:00",
+            "url": "http://www.ibserveis.com/vuelo7.html"
+        },
+        {
+            "origen":"Pekin",
+            "destino": "Londres",
+            "horasalida": "03:00",
+            "horallegada": "17:00",
+            "url": "http://www.ibserveis.com/vuelo8.html"
+        },
+        {
+            "origen":"Turin",
+            "destino": "Malta",
+            "horasalida": "15:00",
+            "horallegada": "16:30",
+            "url": "http://www.ibserveis.com/vuelo9.html"
+        },
+        {
+            "origen":"Lisboa",
+            "destino": "Amsterdam",
+            "horasalida": "08:00",
+            "horallegada": "12:00",
+            "url": "http://www.ibserveis.com/vuelo10.html"
+        }
+    ]
